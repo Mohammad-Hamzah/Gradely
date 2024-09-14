3dadd0da8e85fcd73552caf386b8e041c2f30060
Mohammad Hamzah <mdhamzah7392@gmail.com>
2024-09-14 12:55:41 +0300

    Starting the Project with a basic window

---
+++ tracktive.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+window=tk.Tk()
+#window.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
+window.title("Tracktive")
+window['bg'] = "#36454f"
+window.state('zoomed')
+
+
+window.mainloop()

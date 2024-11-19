38ada8b2857652f06226479e7f88b7a716a2997f
Mohammad Hamzah <mdhamzah7392@gmail.com>
2024-11-19 19:09:51 +0300

    Added bulk update update requirements.txt

--- Gradely.py
+++ Gradely.py
@@ -159,28 +159,29 @@ def admin():
         pass
     def teacher_report():
         pass
+
 def ct(tname,cls):
     while True:
-        typewrite(f"Welcome, {tname}!\nPlease choose from the given options:\n1. Update Marks\n2. Logout\n3. Exit\n> ",color='bold cyan',end='')
+        typewrite(f"Welcome back, {tname}!\nPlease choose from the given options:\n1. Update Marks\n2. Logout\n3. Exit\n> ",color='bold cyan',end='')
         mainchoice=valid_input(['1','2','3'])
         if mainchoice=='1':
             typewrite("Please choose how you would like to update marks (enter '0' for home):\n1. Single Student\n2. Bulk Update\n> ",color='bold cyan',end='')
-            choice=valid_input(['1','2'])
+            choice=valid_input(['0','1','2'])
             if choice=='1':
                 while True:
                     typewrite("Please enter the student's Grno. (enter '0' for home):\n> ",color='bold cyan',end='')
                     grno=int(input())
-                    if grno=='0':
+                    if grno==0:
                         break
                     else:
                         cur.execute("SELECT * FROM STUDENTS WHERE GRNO=%s AND CLASS=%s",(grno,cls))
                         stddata=cur.fetchone()
                         if stddata==None:
-                            typewrite("Data not found! Please try again!:\n> ",color='bold cyan',end='')
+                            typewrite("Data not found! Please try again!:\n> ",color='bold red',end='')
                         else:
                             cur.execute("SELECT SUBJECTS.SUBNAME FROM STUDENTSUBJECTS,SUBJECTS WHERE GRNO=%s AND STUDENTSUBJECTS.SUBID=SUBJECTS.SUBID;",(grno,))
                             subjects=cur.fetchall()
-                            cur.execute("SELECT DISTINCT EXAM FROM EXAMS")
+                            cur.execute("SELECT DISTINCT EXAM FROM EXAMS ORDER BY EXAM")
                             exams=cur.fetchall()
                             s1,s2,s3,s4,s5=subjects[0][0],subjects[1][0],subjects[2][0],subjects[3][0],subjects[4][0]
                             ae,hy,ut1,ut2=exams[0][0],exams[1][0],exams[2][0],exams[3][0]
@@ -190,6 +191,7 @@ def ct(tname,cls):
                             dob=stddata[4]
                             cl=stddata[5]
                             roll=stddata[6]
+                            typewrite(f"Student found...\nGR no.: {grno}\nName: {name}\nRoll no.: {roll}\n",color='bold green',end='')
                             typewrite(f"Please choose the exam (enter '0' for home):\n1. {ut1}\n2. {hy}\n3. {ut2}\n4. {ae}\n> ",color='bold cyan',end='')
                             
                             chosenexam=valid_input(['0','1','2','3','4'])
@@ -204,8 +206,8 @@ def ct(tname,cls):
                                     chosenexam=ut2
                                 if chosenexam=='4':
                                     chosenexam=ae
-                                typewrite(f"Please choose the subject (enter '0' for home):\n1. {s1}\n2. {s2}\n3. {s3}\n4. {s4}\n5. {s5}\n> ",color='bold cyan',end='')
-                                chosensub=valid_input(['0','1','2','3','4','5'])
+                            typewrite(f"Please choose the subject (enter '0' for home):\n1. {s1}\n2. {s2}\n3. {s3}\n4. {s4}\n5. {s5}\n> ",color='bold cyan',end='')
+                            chosensub=valid_input(['0','1','2','3','4','5'])
                             if chosensub=='0':
                                 break
                             else:
@@ -220,25 +222,111 @@ def ct(tname,cls):
                                 if chosensub=='5':
                                     chosensub=s5
                                 
-                                typewrite("Please enter new marks (enter '0' for home):\n> ",color='bold cyan',end='')
-                                newmarks=int(input())
-                                if newmarks==0:
-                                    break
-                                else:
-                                    print(newmarks,grno,chosenexam,chosensub)
-                                    cur.execute("UPDATE MARKS SET MARKS = %s WHERE GRNO = %s AND SUBID = (SELECT SUBID FROM SUBJECTS WHERE SUBNAME = %s) AND EXAM = %s",(newmarks,grno,chosensub,chosenexam))
-                                    db.commit()
-                                    typewrite("Marks updated successfully... Returning to home...\n> ",color='bold green',end='')
-                                    break
+                            typewrite("Please enter new marks (enter '0' for home):\n> ",color='bold cyan',end='')
+                            newmarks=int(input())
+                            if newmarks==0:
+                                break
+                            else:
+                                cur.execute("UPDATE MARKS SET MARKS = %s WHERE GRNO = %s AND SUBID = (SELECT SUBID FROM SUBJECTS WHERE SUBNAME = %s) AND EXAM = %s",(newmarks,grno,chosensub,chosenexam))
+                                db.commit()
+                                typewrite("Marks updated successfully... Returning to home...\n> ",color='bold green',end='')
+                                break
 
-            else:
-                pass
-        elif mainchoice=='2':
+            if choice=='2':
+                while True:
+                    cur.execute("SELECT DISTINCT EXAM FROM EXAMS ORDER BY EXAM")
+                    exams=cur.fetchall()
+                    ae,hy,ut1,ut2=exams[0][0],exams[1][0],exams[2][0],exams[3][0]
+
+                    cur.execute("SELECT DISTINCT SUBNAME FROM STUDENTSUBJECTS,STUDENTS,SUBJECTS WHERE CLASS=%s AND STUDENTS.GRNO=STUDENTSUBJECTS.GRNO AND SUBJECTS.SUBID=STUDENTSUBJECTS.SUBID",(cls,))
+                    subjects=cur.fetchall()
+                    s1,s2,s3,s4,s5=subjects[0][0],subjects[1][0],subjects[2][0],subjects[3][0],subjects[4][0]
+
+
+                    typewrite(f"Please choose the exam for bulk entry (enter '0' for home):\n1. {ut1}\n2. {hy}\n3. {ut2}\n4. {ae}\n> ",color='bold cyan',end='')
+             
+                    chosenexam=valid_input(['0','1','2','3','4'])
+                    if chosenexam=='0':
+                        break
+                    else:
+                        if chosenexam=='1':
+                            chosenexam=ut1
+                        if chosenexam=='2':
+                            chosenexam=hy
+                        if chosenexam=='3':
+                            chosenexam=ut2
+                        if chosenexam=='4':
+                            chosenexam=ae
+
+                    typewrite(f"Please choose the subject for bulk entry (enter '0' for back):\n",end='')
+                    count=1
+
+                    for subject in subjects:
+                        typewrite(f"{count}. {subject[0]}\n",color='bold cyan',end='')
+                        count+=1
+                    typewrite("> ",color='bold cyan',end='')
+                    chosensub=input()
+
+                    if chosensub=='0':
+                        break
+                    else:
+                        while not (chosensub.isdigit() and 1 <= int(chosensub) <= len(subjects)):
+                            typewrite("Please choose from the given options only!\n> ", color='bold yellow', end='')
+                            chosensub=input()
+                            
+                                
+                    chosensub=subjects[int(chosensub)-1][0]
+                    print(chosensub)
+                    cur.execute("SELECT STUDENTS.GRNO FROM STUDENTS WHERE CLASS=%s AND SUBNORDER BY ROLL",(cls,))
+                    stddata=cur.fetchall()
+                    cnt=0
+                    for std in stddata:
+                        grno=std[0]
+                        name=std[1]
+                        father=std[2]
+                        mother=std[3]
+                        dob=std[4]
+                        cl=std[5]
+                        roll=std[6]
+
+                        typewrite(f"GR no.: {grno}\nName: {name}\nRoll no.: {roll}\nEnter Marks ('0' for back): \n> ",color='bold cyan',end='')
+                        newmarks=int(input())
+                    
+                        if newmarks==0:
+                            if cnt>0:
+                                typewrite("Save data entered till now? (Y/N)\n> ",color='bold red',end='')
+                                inp=valid_input(['Y','y','N','n'])
+                                if inp.lower()=='y':
+                                    db.commit()
+                                    typewrite("Changes Saved!",color='bold green',end='')
+                            break
+                        else:
+                            cur.execute("UPDATE MARKS SET MARKS = %s WHERE GRNO = %s AND SUBID = (SELECT SUBID FROM SUBJECTS WHERE SUBNAME = %s) AND EXAM = %s",(newmarks,grno,chosensub,chosenexam))
+                            cnt+=1
+
+                    if count==len(stddata):
+                        typewrite("Marks entered for all students!\nUpdate marks to the database? (Y/N)\n> ",color='bold yellow',end='')
+                        inp=valid_input(['Y','y','N','n'])
+                        if inp.lower()=='y':
+                            db.commit()
+                            typewrite("Marks updated for all students successfully... Returning home!",color='bold green',end='')
+                        
+
+        if mainchoice=='2':
             return
-        else:
+        if mainchoice=='3':
             typewrite("Thanks for using our program!",color='bold green',end='')
             exit()
 #INTRO
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='yoursql',
+    database='gradely'
+)
+cur=db.cursor()
+ct('Mrs. Sheenu Rajesh','12b05')
 
 #asking for sql password and username
 while True:
